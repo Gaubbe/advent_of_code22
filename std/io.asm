fac6b2cd482f20fcaf0ae5324880f6052bc1d628
Gaubbe <gabriel12125@gmail.com>
2022-12-11 18:58:55 -0500

    Added basic file io

--- std/io.asm
+++ std/io.asm
@@ -1,6 +1,8 @@
 global print
+global read_file_to_str
 
 extern strlen
+extern alloc
 
 section .text
 ;	print(
@@ -28,4 +30,74 @@ print:
 
 	mov esp, ebp
 	pop ebp
+	ret
+
+;	read_file_to_str(
+;		file_path: char* [ebp + 8]
+;	) -> str: char*
+;
+;	Reads file and puts result in heap allocated string
+;	file_path must be null-terminated
+; 	Retruns 0 if error occured
+read_file_to_str:
+	push ebp
+	mov ebp, esp
+	push ebx
+
+	; Open the file
+	;-------------------------------------------------------------------------------------
+	mov eax, 5
+	mov ebx, [ebp + 8]
+	xor ecx, ecx ; O_RDONLY
+	xor edx, edx
+	int 80h
+	test eax, eax
+	js fts_error_end
+
+	; Seek the end of the file 
+	;-------------------------------------------------------------------------------------
+	mov ebx, eax
+	mov eax, 19
+	xor ecx, ecx
+	mov edx, 2 ; SEEK_END
+	int 80h
+	
+	; Allocate buffer
+	;-------------------------------------------------------------------------------------
+	inc eax
+	push eax
+	call alloc
+	mov esi, eax
+
+	; Seek back to the beginning
+	;-------------------------------------------------------------------------------------
+	mov eax, 19
+	xor ecx, ecx
+	xor edx, edx ; SEEK_SET
+	int 80h
+	
+	; Read file into buffer
+	;-------------------------------------------------------------------------------------
+	mov eax, 3
+	mov ecx, esi
+	pop edx
+	dec edx
+	int 80h
+	
+	; Close file
+	;-------------------------------------------------------------------------------------
+	mov eax, 6
+	int 80h
+
+	mov eax, esi
+	pop ebx
+	mov esp, ebp
+	pop ebp
+	ret
+
+	fts_error_end:
+	pop ebx
+	mov esp, ebp
+	pop ebp
+	xor eax, eax
 	ret
