7da1e5be2e342ee547c56129f792a7f11d19284f
Gaubbe <gabriel12125@gmail.com>
2022-12-11 17:37:02 -0500

    Added strcopy

--- std/strings.asm
+++ std/strings.asm
@@ -1,4 +1,5 @@
 global strlen
+global strcopy
 
 section .text
 
@@ -14,14 +15,40 @@ strlen:
 	xor eax, eax ; eax <- couter: int = 0
 	mov ecx, [ebp + 8] ; ecx <- current_char_ptr: char* = str
 
-	loop:
+	len_loop:
 	mov dl, byte [ecx + eax] ; edx <- current_char: char = *(current_char_ptr + counter)
 	test dl, dl
-	jz end
+	jz len_end
 	inc eax
-	jmp loop
+	jmp len_loop
 
-	end:
+	len_end:
+	mov esp, ebp
+	pop ebp
+	ret
+
+;	strcopy(
+;		orig: char* [ebp+8],
+;		dest: char* [ebp+12]
+;	) void
+;	Copies string form orig to dest. orig must be null terminated
+strcopy:
+	push ebp
+	mov ebp, esp
+
+	mov eax, [ebp + 8] ; eax <- current_char_ptr: char* = orig
+	mov ecx, [ebp + 12] ; ecx <- dest_char_ptr: char* = dest
+
+	cpy_loop:
+	mov dl, byte [eax]
+	mov byte [ecx], dl
+	test dl, dl
+	jz cpy_end
+	inc eax
+	inc ecx
+	jmp cpy_loop
+
+	cpy_end:
 	mov esp, ebp
 	pop ebp
 	ret
